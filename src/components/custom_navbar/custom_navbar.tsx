964a280356a53645a25788e9f4a55026d64752fa
Anthony J. DeArmas <anthonyjdearmas@gmail.com>
2023-12-29 12:18:55 -0500

    Added about me section

--- src/components/custom_navbar/custom_navbar.tsx
+++ src/components/custom_navbar/custom_navbar.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useState, useRef } from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import './custom_navbar.css';
@@ -10,26 +10,44 @@ interface CustomNavbarProps { }
 
 const CustomNavbar: FC<CustomNavbarProps> = () => {
   const [activeLink, setActiveLink] = useState('home');
+  const aboutRef = useRef<HTMLDivElement>(null);
+  let onAboutMe = false;
 
   const handleLinkClick = (link: string) => {
     setActiveLink(link);
+    if (link === 'about' && aboutRef.current) {
+      window.scrollTo({
+        top: document.documentElement.scrollHeight,
+        behavior: 'smooth',
+      });
+      onAboutMe = true;
+    } else {
+      onAboutMe = false;
+    }
+
+    if (link === 'home' && !onAboutMe) {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      });
+    }
   };
 
   return (
-    <Navbar expand="lg" variant="dark" id="navbar_bg">
-      <Navbar.Brand href="index.html">
+    <Navbar expand="lg" variant="dark" id="navbar_bg" style={{ position: 'sticky', top: 0, zIndex: 100 }}>
+      <Navbar.Brand href="/">
         <h1 id="navbar_title"><strong>Anthony DeArmas</strong></h1>
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="navbarTogglerDemo02" />
       <Navbar.Collapse id="navbarTogglerDemo02">
         <Nav className="ms-auto" id="nav_bundle">
           <Nav.Item className={activeLink === 'home' ? 'active' : ''}>
-            <Nav.Link as={Link} to="/home" id="nav-link1" onClick={() => handleLinkClick('home')}>
+            <Nav.Link id="nav-link1" onClick={() => handleLinkClick('home')}>
               Home
             </Nav.Link>
           </Nav.Item>
           <Nav.Item className={activeLink === 'about' ? 'active' : ''}>
-            <Nav.Link as={Link} to="/about" id="nav-link2" onClick={() => handleLinkClick('about')}>
+            <Nav.Link id="nav-link2" onClick={() => handleLinkClick('about')}>
               About Me
             </Nav.Link>
           </Nav.Item>
@@ -45,6 +63,7 @@ const CustomNavbar: FC<CustomNavbarProps> = () => {
           </Nav.Item>
         </Nav>
       </Navbar.Collapse>
+      <div ref={aboutRef} />
     </Navbar>
   );
 };
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,36 +4,54 @@ import './Home.css';
 interface HomeProps { }
 
 const Home: FC<HomeProps> = () => (
-  <div className="container">
-    <div className="row mt-5">
-      <div className="col-6">
-        <div id="textbox" className="p-3 rounded">
-          <h2 id="intro_title">Welcome,</h2>
-          <p id="intro_text">
-            My name is Anthony DeArmas and I'm a full stack software engineer based in New York City.
-            I'm passionate about creating web applications that is both intuitive and efficient.
-            Here you can find some of my projects and learn more about me. Click the icons below to
-            visit my GitHub and LinkedIn profiles, or click the resume icon to view my resume.
-          </p>
-          <div className="container text-center" style={{ height: '100px' }}>
-            <div className="row">
-              <div>
-                <a href="https://github.com/anthonyjdearmas" target="_blank">
-                  <img id="icon-git" className="img-fluid mt-3 icon" src="img/icons/github.png" alt="GitHub Icon" />
-                </a>
-                <a href="https://www.linkedin.com/in/ajd249/" target="_blank">
-                  <img id="icon-lnki" className="img-fluid mt-3 icon" src="img/icons/linkedin.png" alt="LinkedIn Icon" />
-                </a>
-                <a href="pdf/cs_newresume.pdf" target="_blank">
-                  <img id="icon-res" className="img-fluid mt-3 icon" src="img/icons/resume.png" alt="Resume Icon" />
-                </a>
+  <div>
+
+    <div className="container" id="intro_section">
+      <div className="row mt-5">
+        <div className="col-6">
+          <div>
+            <div id="textbox" className="p-3 rounded">
+              <h2 id="intro_title">Welcome,</h2>
+              <p id="intro_text">
+                My name is Anthony DeArmas  and I'm a full stack software engineer based in New York City.
+                I'm passionate about creating web applications that is both intuitive and efficient.
+                Here you can find some of my projects and learn more about me. Click the icons below to
+                visit my GitHub and LinkedIn profiles, or click the resume icon to view my resume.
+              </p>
+              <div className="container text-center" style={{ height: '100px' }}>
+                <div className="row">
+                  <div>
+                    <a href="https://github.com/anthonyjdearmas" target="_blank">
+                      <img id="icon-git" className="img-fluid mt-3 icon" src="img/icons/github.png" alt="GitHub Icon" />
+                    </a>
+                    <a href="https://www.linkedin.com/in/ajd249/" target="_blank">
+                      <img id="icon-lnki" className="img-fluid mt-3 icon" src="img/icons/linkedin.png" alt="LinkedIn Icon" />
+                    </a>
+                    <a href="pdf/cs_newresume.pdf" target="_blank">
+                      <img id="icon-res" className="img-fluid mt-3 icon" src="img/icons/resume.png" alt="Resume Icon" />
+                    </a>
+                  </div>
+                </div>
               </div>
             </div>
+            <img src="/img/intro.jpg" alt="Intro" id="intro_img" />
           </div>
         </div>
-        <img src="/img/intro.jpg" alt="Intro" id="intro_img" />
       </div>
     </div>
+
+
+    <div className="container mb-5" id="about_me">
+      <div className="row">
+        <div className="col-6">
+          <img src="/img/abpic.jpg" alt="About" id="about_img" className="img-fluid rounded" />
+        </div>
+        <div className="col-6">
+          <h2 className="text-center mt-5 mb-5">About me</h2>
+        </div>
+      </div>
+    </div>
+
   </div>
 );
 
