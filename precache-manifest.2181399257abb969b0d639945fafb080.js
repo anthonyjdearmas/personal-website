953d4135ecb94daa5347e28238b55f3b7aee249c
Anthony J. DeArmas <anthonyjdearmas@gmail.com>
2024-02-04 20:28:11 -0500

    Updates

---
+++ precache-manifest.2181399257abb969b0d639945fafb080.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9bff7131d9566260d645",
+    "url": "/new-personal-website/static/css/main.76c090ac.chunk.css"
+  },
+  {
+    "revision": "9bff7131d9566260d645",
+    "url": "/new-personal-website/static/js/main.9bff7131.chunk.js"
+  },
+  {
+    "revision": "9df7b8f4a82597eefd5c",
+    "url": "/new-personal-website/static/js/1.9df7b8f4.chunk.js"
+  },
+  {
+    "revision": "f0e467ca4e59c47abd4f",
+    "url": "/new-personal-website/static/css/2.b0902ae3.chunk.css"
+  },
+  {
+    "revision": "f0e467ca4e59c47abd4f",
+    "url": "/new-personal-website/static/js/2.f0e467ca.chunk.js"
+  },
+  {
+    "revision": "327f8728518c2c6b5bb6",
+    "url": "/new-personal-website/static/js/runtime~main.327f8728.js"
+  },
+  {
+    "revision": "d796e8e957488f7aba20c5cf03b7d86e",
+    "url": "/new-personal-website/static/media/webscraping.d796e8e9.mp4"
+  },
+  {
+    "revision": "59ad2899609c622ac924c0491aafec56",
+    "url": "/new-personal-website/index.html"
+  }
+];
