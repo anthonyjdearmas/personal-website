3f63c673a967c5ebf58ce8f6f400890d53aafaca
Anthony J. DeArmas <anthonyjdearmas@gmail.com>
2024-02-04 20:34:55 -0500

    Updates

---
+++ precache-manifest.a4579f045957252685e52c3e20903027.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9bff7131d9566260d645",
+    "url": "/personal-website/static/css/main.76c090ac.chunk.css"
+  },
+  {
+    "revision": "9bff7131d9566260d645",
+    "url": "/personal-website/static/js/main.9bff7131.chunk.js"
+  },
+  {
+    "revision": "9df7b8f4a82597eefd5c",
+    "url": "/personal-website/static/js/1.9df7b8f4.chunk.js"
+  },
+  {
+    "revision": "f0e467ca4e59c47abd4f",
+    "url": "/personal-website/static/css/2.b0902ae3.chunk.css"
+  },
+  {
+    "revision": "f0e467ca4e59c47abd4f",
+    "url": "/personal-website/static/js/2.f0e467ca.chunk.js"
+  },
+  {
+    "revision": "c39532cd52b5a265f3c8",
+    "url": "/personal-website/static/js/runtime~main.c39532cd.js"
+  },
+  {
+    "revision": "d796e8e957488f7aba20c5cf03b7d86e",
+    "url": "/personal-website/static/media/webscraping.d796e8e9.mp4"
+  },
+  {
+    "revision": "6eee61101fe30dfac3357e79b1d1dff9",
+    "url": "/personal-website/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/new-personal-website/precache-manifest.2181399257abb969b0d639945fafb080.js"
+  "/personal-website/precache-manifest.a4579f045957252685e52c3e20903027.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/new-personal-website/index.html", {
+workbox.routing.registerNavigationRoute("/personal-website/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
