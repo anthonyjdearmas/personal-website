4a0cf9ce4ca6183098f2147d6698e2d49263267f
Anthony J. DeArmas <anthonyjdearmas@gmail.com>
2024-02-08 15:49:39 -0500

    update about me

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -89,32 +89,36 @@ const Home: FC<HomeProps> = () => (
           <h2 className="text-center mt-3">About Me</h2>
           <hr />
           <p>
-            I'm a recent graduate from Cornell University with a deep passion for software development.
-            Throughout my academic journey, Cornell provided not just a degree but a robust foundation for
-            excellence in the tech world.
+            I'm a full stack software engineer who graduated from Cornell University with a degree in Computer Science.
+            I currently work at FTI Consulting where I help to create specialized software solutions for multiple clients
+            concurrently. Below I discuss my journey to becoming a software engineer and some of the projects I've worked on.
           </p>
           <h3 className="mt-5"><i>Teaching and Learning at iD Tech</i></h3>
           <p>
-            During college, I found fulfillment in teaching programming at iD Tech, where I connected with students
+            During college, I found fulfillment in teaching part time at iD Tech, where I connected with students
             aged 10-17. This experience enhanced my communication skills and laid the groundwork for translating
             complex concepts into accessible lessons.
           </p>
+          <h3><i>Collaborating with Other Students</i></h3>
+          <p>
+            Over the summer of my sophmore year, I became part of a project named AI-Learners. This project was a collaboration
+            with other Cornell students where I led a small team to develop a web application to help children with special needs learn.
+            The project later became funded by Cornell e-Labs and continues to grow today. It can be visited at:
+            <a href="https://ai-learners.com/" target="_blank"> AI-Learners</a>
+          </p>
           <h3><i>Internship to Full-Time Role at FTI Consulting</i></h3>
           <p>
-            Post-graduation, my journey led me to FTI Consulting, where an enriching full-stack software developer internship
-            paved the way for a full-time role. Collaborating on impactful projects and problem-solving in a dynamic environment
-            became the hallmark of my professional journey.
+            During my junior year I interned at FTI Consulting, where I learned to become a professional software engineer.
+            There I learned in leaps and bounds about the software development process and how to work with modern technologies.
+            I was later offered a full-time role as a software engineer after graduation and am currently working there.
+            Now I am part of multiple projects at the same time, communicate with clients and am growing into a better developer
+            everyday.
           </p>
           <h3><i>Passion for Projects</i></h3>
           <p>
             Beyond my 9-to-5 role, I channel my enthusiasm into personal projects. A recent endeavor is an online food
             safety training website. Users can study, track progress, and attain certification, showcasing my belief
             in technology's practical applications. You can visit it here: <a href="https://safeservemetro.com/" target="_blank">SafeServe Metro</a>
-          </p>
-          <h3><i>Commitment to Growth</i></h3>
-          <p>
-            Continuous learning isn't just a professional obligation; it's a personal commitment. Staying current with emerging technologies
-            fuels my eagerness for new challenges and ensures my contributions remain relevant in the ever-evolving software development landscape.
           </p>
         </div>
       </div>
